a400260eea5594a171f08efcc8c464420ccd0145
xxdydx <official@arul.me>
2023-01-17 13:43:10 +0800

    lol

---
+++ models/form.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  content: String,
+  section: String,
+  answer: String,
+  quantity: String,
+  remarks: String,
+});
+questionSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const formSchema = new mongoose.Schema({
+  title: String,
+  type: String,
+  dateSubmitted: Date,
+  dateOfForm: Date,
+  logo: String,
+  signature: String,
+  name: String,
+  company: String,
+  jobTitle: String,
+  location: String,
+  questions: [questionSchema],
+});
+formSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Form", formSchema);
